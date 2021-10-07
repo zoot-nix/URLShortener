07a0bca42b345f3652c0ff4fab325989c35b7fdd
Owais Shaikh <66899497+zoot-nix@users.noreply.github.com>
2021-10-07 17:17:03 +0530

    Added Domain

--- UrlShortener/settings.py
+++ UrlShortener/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--wmhrbqcx6a)y%+d%*jd#5ng@4@3!)t$z#)93k1=5lp2f*=&8!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'zturlshortener.herokuapp.com']
 
 
 # Application definition
